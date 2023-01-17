2511b638d31b7d0d8596cc847c4e6e5d09f88acd
João vitor <contato.joaovitorscr@gmail.com>
2023-01-17 10:53:36 -0300

    fix

--- GruntFile.js
+++ GruntFile.js
@@ -1,28 +1,28 @@
 const { task } = require("grunt");
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
         less: {
             production: {
                 options: {
                     compress: true,
                 },
                 files: {
-                    'dist/styles/main.min.css' : 'src/styles/main.less'
-                }
-            },
-        uglify: {
-            target: {
-                files: {
-                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
-                    }
-                }
+                    "dist/styles/main.min.css": "src/styles/main.less",
+                },
             }
-        }
-    })
+        },
+            uglify: {
+                target: {
+                    files: {
+                        "dist/scripts/main.min.js": "src/scripts/main.js",
+                    },
+                },
+            },
+    });
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.registerTask('default', ['less:production', 'uglify']);
-}
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.registerTask("default", ["less:production", "uglify"]);
+};
