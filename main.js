45172a138f07a0c01ede8b47430eb8ed200f3a42
João vitor <contato.joaovitorscr@gmail.com>
2023-02-13 16:20:01 -0300

    init

---
+++ main.js
@@ -0,0 +1,4 @@
+var multiplicacao = function (primeiroNumero, segundoNumero) { return primeiroNumero * segundoNumero; };
+console.log(multiplicacao(10, 10));
+var dizOla = function (nome) { return 'Olá ' + nome; };
+console.log(dizOla('João'));
---
+++ main.ts
@@ -0,0 +1,5 @@
+const multiplicacao = (primeiroNumero: number, segundoNumero: number): number => primeiroNumero * segundoNumero
+console.log(multiplicacao(10, 10))
+
+const dizOla = (nome: string): string => 'Olá ' + nome
+console.log(dizOla('João'))
